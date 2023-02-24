8a24f3c4e78253fd2376c35cddb23798eb91be31
ungsintra <mamirov.21@mail.ru>
2023-02-24 21:42:41 +0300

    import app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+<StrictMode>
+        <App />
+    </StrictMode>
 );
 
