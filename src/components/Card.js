49c35c7fa914f74b0e8fdf0b694078dd061d02fd
ungsintra <mamirov.21@mail.ru>
2023-02-24 21:42:03 +0300

    added Card cmp and his fn

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
-const Card = ({ card, onCardClickHandler }) => {
+const Card = ({ card, onCardClickHandler, onClickLike, onClickDelete }) => {
+  const currentUser = useContext(CurrentUserContext);
   const hadleClick = () => {
     onCardClickHandler(card);
   };
 
+  const hadleClickLike = () => {
+    onClickLike(card);
+  };
+
+  const handleCardDelete = () => {
+    onClickDelete(card);
+  };
+  const isLikes = card.likes.some((like) => {
+    return like._id === currentUser.id;
+  });
+  const isOwn = currentUser.id === card.owner._id;
   return (
     <>
-      <li className="place__item">
-        <button
-          type="button"
-          aria-label="Удалить"
-          className="place__btn-del"
-        ></button>
+      <li className="place__item" key={card._id}>
+        {isOwn && (
+          <button
+            type="button"
+            aria-label="Удалить"
+            style={{ display: "block" }}
+            className={`place__btn-del`}
+            onClick={handleCardDelete}
+          ></button>
+        )}
         <div
           onClick={hadleClick}
           src=""
@@ -25,7 +42,10 @@ const Card = ({ card, onCardClickHandler }) => {
           <button
             type="button"
             aria-label="Нравиться"
-            className="place__btn-like"
+            onClick={hadleClickLike}
+            className={`place__btn-like ${
+              isLikes && "place__btn-like_status_liked"
+            }`}
           ></button>
           <span className="place__like-count">{card.likes.length}</span>
         </div>
