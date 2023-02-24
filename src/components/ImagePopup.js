a76c38de0c4c0adb2fa95b8d08172c2c862fbc53
ungsintra <mamirov.21@mail.ru>
2023-02-24 21:44:46 +0300

    added image popup

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from "react";
 
-const ImagePopup = ({card,onClose}) => {
-    return (
-        <div
-         onClick={onClose}
-         className={`popup popup_use_img ${(card.isOpen) ? `popup__opened`: ''}`}>
-        <div className="popup__content_use_img">
-          <button
-            onClick={onClose}
-            type="button"
-            aria-label="Закрыть попап"
-            className="popup__close-btn popup__close-btn-img"
-          ></button>
-            <img src={card.link} alt="" className="popup__img" />
-          <p className="popup__desc">{card.name}</p>
-        </div>
+const ImagePopup = ({ card, onClose }) => {
+  return (
+    <div
+      onMouseDown={onClose}
+      className={`popup popup_use_img ${card.isOpen ? `popup__opened` : ""}`}
+    >
+      <div className="popup__content_use_img">
+        <button
+          onClick={onClose}
+          type="button"
+          aria-label="Закрыть попап"
+          className="popup__close-btn popup__close-btn-img"
+        ></button>
+        <img src={card.link} alt="" className="popup__img" />
+        <p className="popup__desc">{card.name}</p>
       </div>
-  
-    );
+    </div>
+  );
 };
 
 export default ImagePopup;
