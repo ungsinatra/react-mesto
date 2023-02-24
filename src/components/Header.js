cbdee94e5dcb75c868c148b24493f9bdccf3935e
ungsintra <mamirov.21@mail.ru>
2023-02-24 21:44:24 +0300

    added header cmp and import deps

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import headerLogo from '../images/logo.svg';
+import React from "react";
+import headerLogo from "../images/logo.svg";
 
 const Header = () => {
-    return (
-        <header className="header">
-        <img src={headerLogo} className="header__logo" alt="Логотип шапки" />
-      </header>
-    );
+  return (
+    <header className="header">
+      <img src={headerLogo} className="header__logo" alt="Логотип шапки" />
+    </header>
+  );
 };
 
 export default Header;
