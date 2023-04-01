f66fbbb86855d0bb1707824bfbb89279ea9b7b8c
ungsintra <mamirov.21@mail.ru>
2023-04-01 12:24:30 +0300

    change some in main

---
+++ src/components/layouts/Main.js
@@ -0,0 +1,65 @@
+import React, { useContext } from "react";
+import Card from "../widgets/Card";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext.js";
+
+const Main = ({cards,handlers}) => {
+  const userContext = useContext(CurrentUserContext);
+
+  return (
+    <main>
+      <section className="profile">
+        <div className="profile__wrapper">
+          <div className="profile__items">
+            <div className="profile__img-items">
+              <div
+                className="profile__avatar-switch-ican"
+                onClick={handlers.onEditAvatar}
+              ></div>
+              <div
+                style={{ backgroundImage: `url(${userContext.userAvatar})` }}
+                className="profile__avatar"
+              ></div>
+            </div>
+
+            <div className="profile__info">
+              <button
+                className="profile__edit-btn"
+                type="button"
+                area-label="Изменить"
+                onClick={handlers.onEditProfile}
+              ></button>
+              <h1 className="profile__name">{userContext.userName}</h1>
+              <p className="profile__description">{userContext.userDescription}</p>
+            </div>
+          </div>
+          <button
+            type="button"
+            aria-label="Добавить"
+            className="profile__add-btn"
+            onClick={handlers.onAddPlace}
+          ></button>
+        </div>
+      </section>
+      <section className="place">
+        <ul className="place__container">
+          {cards.map((card, index) => (
+            <Card
+              card={card}
+              key={card._id}
+              onClickLike = {handlers.onClickLike}
+              onCardClickHandler={handlers.onCardClick}
+              onClickDelete = {handlers.onClickDelete}
+            />
+          ))}
+        </ul>
+      </section>
+
+
+
+
+
+    </main>
+  );
+};
+
+export default Main;
