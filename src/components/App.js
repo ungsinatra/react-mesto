45020ddbf389a9f78eff15e86bf74c5cc9b958ac
ungsintra <mamirov.21@mail.ru>
2023-04-01 12:23:05 +0300

    change directory

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from "react";
-import EditProfilePopup from "./EditProfilePopup.js";
-import Footer from "./Footer.js";
-import Header from "./Header.js";
-import DeleteCardPopup from "./DeleteCardPopup.js";
-import EditAvatarPopup from "./EditAvatarPopup.js";
-import Main from "./Main.js";
-import ImagePopup from "./ImagePopup.js";
+import EditProfilePopup from "./modals/EditProfilePopup.js";
+import Footer from "./layouts/Footer.js";
+import Header from "./layouts/Header.js";
+import DeleteCardPopup from "./modals/DeleteCardPopup.js";
+import EditAvatarPopup from "./modals/EditAvatarPopup.js";
+import Main from "./layouts/Main.js";
+import ImagePopup from "./modals/ImagePopup.js";
 import { api } from "../utils/api.js";
 import { CurrentUserContext } from "../contexts/CurrentUserContext.js";
-import AddPlacePopup from "./AddPlacePopup.js";
-
+import AddPlacePopup from "./modals/AddPlacePopup.js";
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
+import Login from "./layouts/Login.js";
+import Register from "./layouts/Register.js";
+import ProtectedRoute from "./widgets/ProtectedRoute.js";
+import { verifiedToken } from "../utils/auth.js";
+import InfoTooltip from "./modals/InfoTooltip.js";
 
 function App() {
+  
   const handleCardClick = (data) => {
     setSelectedCard({
       ...selectedCard,
@@ -49,17 +55,29 @@ function App() {
     if (isDellCardPopup.isOpen) {
       setDellCardPopup({
         ...isDellCardPopup,
-        isOpen:!isDellCardPopup.isOpen
+        isOpen: !isDellCardPopup.isOpen,
       });
     }
+    if(authSuccess){
+      setAuthSuccess(false)
+    }
+    if(authError){
+      setAuthError(false)
+    }
   };
-
   const [currentUser, setCurrentUser] = useState({
+
     userAvatar: "",
     userName: "",
     userDescription: "",
+    id: "",
+  });
+
+  const [userData, setUserData] = useState({
+    userName: "",
     _id: "",
   });
+
   const [cards, setCards] = useState([]);
   const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = useState(false);
   const [isAddPlacePopupOpen, setAddPlacePopupOpen] = useState(false);
@@ -70,10 +88,14 @@ function App() {
     isOpen: false,
   });
   const [isLoading, setLoading] = useState(false);
+  const [authSuccess,setAuthSuccess] = useState(false);
+  const [authError,setAuthError] = useState(false)
   const [isDellCardPopup, setDellCardPopup] = useState({
-    _id:null,
+    _id: null,
     isOpen: false,
   });
+  const [loggedIn, setLoggedIn] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     function closeFromEscape(evt) {
@@ -81,14 +103,30 @@ function App() {
         closeAllPopups();
       }
     }
-
+    console.log(cards);
     document.addEventListener("keydown", closeFromEscape);
 
     return () => {
       document.removeEventListener("keydown", closeFromEscape);
     };
   });
+  function hanldeAuthSucc(){
+      setAuthSuccess(true);
+  }
 
+  function hanldeAuthError(){
+    setAuthError(true)
+  }
+
+  function handleLoggedIn(status) {
+    setLoggedIn(status);
+  }
+  function handlerClearUserData() {
+    setUserData({
+      userName: "",
+      _id: "",
+    });
+  }
   useEffect(() => {
     Promise.all([api.getUserInfo(), api.getCards()]).then(
       ([userInfo, cardsList]) => {
@@ -104,20 +142,16 @@ function App() {
   }, []);
 
   const handleCardDeleteClick = (data) => {
-    console.log(data)
+    console.log(data);
     setDellCardPopup((state) => {
-      const newData = {...state};
+      const newData = { ...state };
       newData.isOpen = true;
       newData._id = data._id;
       return newData;
-
     });
-
-    
   };
 
-
-  // fixed
+ 
   function handleCardLike(card) {
     const isLiked = card.likes.some((like) => like._id === currentUser.id);
     if (!isLiked) {
@@ -146,8 +180,27 @@ function App() {
     }
   }
 
+  useEffect(() => {
+    checkToken();
+  }, []);
+
+  function checkToken() {
+    const jwt = localStorage.getItem("jwt");
+    if (jwt) {
+      setLoggedIn(true);
+      verifiedToken(jwt).then((res) => {
+        console.log(res)
+        setUserData({
+          userName: res.data.email,
+          _id: res.data._id,
+        });
+        navigate("/", { replace: true });
+      });
+    } 
+  }
+
   function handleCardDelete(card) {
-    console.log(card)
+    console.log(card);
     handleloading(true);
     const deleteCard = api.deleteCard(card._id).then((res) => {
       setCards((state) => {
@@ -158,7 +211,7 @@ function App() {
       });
       setDellCardPopup({
         ...isDellCardPopup,
-        isOpen:false
+        isOpen: false,
       });
     });
     deleteCard.catch((res) => {
@@ -227,23 +280,48 @@ function App() {
   function handleloading(status) {
     setLoading(status);
   }
-
+  console.log(currentUser)
   return (
     <div className="page">
       <CurrentUserContext.Provider value={currentUser}>
-        <Header />
-        <Main
-          cards={cards}
-          handlers={{
-            onCardClick: handleCardClick,
-            closePoupsHandler: closeAllPopups,
-            onEditAvatar: handleEditAvatarClick,
-            onAddPlace: handleAddPlaceClick,
-            onEditProfile: handleEditProfileClick,
-            onClickLike: handleCardLike,
-            onClickDelete: handleCardDeleteClick,
-          }}
+        <Header
+          loggedIn={loggedIn}
+          userName={userData.userName}
+          onClearUserData={handlerClearUserData}
+          onLoggedIn = {handleLoggedIn}
         />
+        <Routes>
+          <Route
+            path="/"
+            element={loggedIn?<Navigate to = '/places'  replace/>:<Navigate to = '/sing-in' replace/>}
+          />
+          <Route
+            path="/sing-in"
+            element={<Login onChangeLogin={handleLoggedIn} isOpen = {authSuccess} onAuthSucc = {hanldeAuthSucc} onAuthError = {hanldeAuthError} userData = {userData} onUserData = {setUserData} />}
+          />
+          <Route path="/sing-up" element={<Register onChangeLogin={handleLoggedIn} isOpen = {authSuccess} onAuthSucc = {hanldeAuthSucc} onAuthError = {hanldeAuthError} />} />
+          <Route
+            path="/places"
+            element={
+              <ProtectedRoute
+                loggedIn={loggedIn}
+                element={Main}
+                cards={cards}
+                handlers={{
+                  onCardClick: handleCardClick,
+                  closePoupsHandler: closeAllPopups,
+                  onAddPlace: handleAddPlaceClick,
+                  onEditAvatar: handleEditAvatarClick,
+                  onEditProfile: handleEditProfileClick,
+                  onClickLike: handleCardLike,
+                  onClickDelete: handleCardDeleteClick,
+                }}
+              />
+            }
+          />
+        </Routes>
+        {loggedIn&&<Footer />}
+
         <ImagePopup card={selectedCard} onClose={closeAllPopups} />
         <EditProfilePopup
           isOpen={isEditProfilePopupOpen}
@@ -270,7 +348,10 @@ function App() {
           isOpen={isDellCardPopup.isOpen}
           onSubmit={handleCardDelete}
         />
-        <Footer />
+        <InfoTooltip name = 'auth-success' isOpen={authSuccess} onClose = {closeAllPopups} title = 'Вы успешно зарегистрировались!'/>
+        <InfoTooltip name = 'auth-error' isOpen={authError} onClose = {closeAllPopups} title = {`Что-то пошло не так!
+Попробуйте ещё раз.`}/>
+
       </CurrentUserContext.Provider>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import {BrowserRouter} from  'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<StrictMode>
+    <BrowserRouter>
         <App />
-    </StrictMode>
+    </BrowserRouter>
 );
 
